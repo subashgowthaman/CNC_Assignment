dd6f9e6fc38c1103f51d8802c31c57065936ac82
subashgowthaman <41564306+subashgowthaman@users.noreply.github.com>
2020-08-17 01:47:23 +0530

    Uploaded files from IDE

---
+++ Validator.cpp
@@ -0,0 +1,23 @@
+
+
+
+using namespace std;
+
+bool isBetweenPassingRange(double value_in, pair<float,float> range)
+{
+    bool passStatus = false;
+
+    if((value_in < range.first) || (value_in > ranage.second))
+    {
+        passStatus = false;
+    }
+    else 
+    {
+    	passStatus = true;
+    }
+
+    return passStatus;
+}
+
+
+
