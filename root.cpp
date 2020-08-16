b439a583c4d8b3c673bf741c68c339968731fffe
subashgowthaman <41564306+subashgowthaman@users.noreply.github.com>
2020-08-17 01:48:28 +0530

    remove analmolies

--- root.cpp
+++ root.cpp
@@ -13,29 +13,23 @@ int main()
     
     TemperatureInCelcius.SetOperatingTemperature(28.0F);
     TemperatureInCelcius.SetOperatingTemperature(29.0F);
-    TemperatureInCelcius.SetOperatingTemperature(30.0F);
-    TemperatureInCelcius.SetOperatingTemperature(35.0F);
+
     TemperatureInCelcius.SetOperatingTemperature(34.0F);
-    TemperatureInCelcius.SetOperatingTemperature(35.0F);
-    TemperatureInCelcius.SetOperatingTemperature(36.0F);
+
     TemperatureInCelcius.SetOperatingTemperature(34.0F);
     TemperatureInCelcius.SetOperatingTemperature(35.0F);
 
     OperatingTimeInMinutes.SetElapsedMachineOperationTime(15.0f);
     OperatingTimeInMinutes.SetElapsedMachineOperationTime(25.0f);
-    OperatingTimeInMinutes.SetElapsedMachineOperationTime(35.0f);
-    OperatingTimeInMinutes.SetElapsedMachineOperationTime(15.0f);    
-    OperatingTimeInMinutes.SetElapsedMachineOperationTime(150.0f);
+
     OperatingTimeInMinutes.SetElapsedMachineOperationTime(250.0f);
     OperatingTimeInMinutes.SetElapsedMachineOperationTime(350.0f);
+	
     OperatingTimeInMinutes.SetElapsedMachineOperationTime(360.0f);
     OperatingTimeInMinutes.SetElapsedMachineOperationTime(365.0f);
 
     CuttingToolDimensionInMm.SetCuttingToolVariationNow(0.002f);
-    CuttingToolDimensionInMm.SetCuttingToolVariationNow(0.004f);
-    CuttingToolDimensionInMm.SetCuttingToolVariationNow(0.008f);
-    CuttingToolDimensionInMm.SetCuttingToolVariationNow(0.01f);
-    CuttingToolDimensionInMm.SetCuttingToolVariationNow(0.02f);
+
     CuttingToolDimensionInMm.SetCuttingToolVariationNow(0.035f);
     CuttingToolDimensionInMm.SetCuttingToolVariationNow(0.045f);
     CuttingToolDimensionInMm.SetCuttingToolVariationNow(0.05f);
